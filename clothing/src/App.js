ae4d1e365ffb4c5a0beaa7af03f654fcd44c1cc5
prasadavaghan <prasadavaghan26@gmail.com>
2022-03-03 15:42:27 +0530

    clothing page Added

---
+++ clothing/src/App.js
@@ -0,0 +1,14 @@
+
+
+import { Products } from './components/Products';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
